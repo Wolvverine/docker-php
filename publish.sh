b5bfb117f40960ac6b8cb53a82d8a2593f9c427f
Michał Panasiewicz <wolvverinepld@gmail.com>
2020-02-17 19:51:18 +0100

    correct publish

--- publish.sh
+++ publish.sh
@@ -26,12 +26,12 @@ if [[ -z "$DOCKERHUB_REGISTRY_USERNAME" || -z "$DOCKERHUB_REGISTRY_PASSWORD" ]];
   exit 1
 fi
 
-image_version=`cat VERSION`
+image_version=${VERSION}
 
-if [[ -n ${IMAGE_VARIANT} ]]; then
-  image_building_name="${DOCKER_IMAGE}:building_${IMAGE_VARIANT}"
-  image_tags_prefix="${IMAGE_VARIANT}-"
-  echo "-> set image variant '${IMAGE_VARIANT}' for build"
+if [[ -n ${VARIANT} ]]; then
+  image_building_name="${DOCKER_IMAGE}:building_${VARIANT}"
+  image_tags_prefix="${VARIANT}-"
+  echo "-> set image variant '${VARIANT}' for build"
 else
   image_building_name="${DOCKER_IMAGE}:building"
 fi
