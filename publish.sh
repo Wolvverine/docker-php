5a43ecc324754b596b12f9f8be0a2305231751ae
Michał Panasiewicz <wolvverinepld@gmail.com>
2020-02-18 10:41:17 +0100

    correct

--- publish.sh
+++ publish.sh
@@ -15,20 +15,20 @@ set -x
 if [[ "${DOCKER_REPO}" =~ ([^/]+)/([^/]+) ]]; then
   username="${BASH_REMATCH[1]}";
   repo="${BASH_REMATCH[2]}";
-  echo "-> set username to ${username}"
-  echo "-> set repository to ${repo}"
+  echo "-> set username to ${username}";
+  echo "-> set repository to ${repo}";
 else
-  echo 'ERROR: unable to extract username and repo from environment' 1>&2
+  echo "ERROR: unable to extract username and repo from environment" 1>&2;
   exit 1
 fi
 
 if [[ -z "$DOCKERHUB_REGISTRY_USERNAME" || -z "$DOCKERHUB_REGISTRY_PASSWORD" ]]; then
-  echo 'ERROR: missing one of the registry credential DOCKERHUB_REGISTRY_USERNAME DOCKERHUB_REGISTRY_PASSWORD' 1>&2
+  echo 'ERROR: missing one of the registry credential DOCKERHUB_REGISTRY_USERNAME DOCKERHUB_REGISTRY_PASSWORD' 1>&2 ;
   exit 1
 fi
 
 image_version="${VERSION}"
-VARIANT_TAG="${VARIANT/\//-}" ;
+VARIANT_TAG="`${VARIANT/\//-}`" ;
 
 if [[ -n "${VARIANT}" ]]; then
   cd "~/build/Wolvverine/docker-php/${VERSION}/${VARIANT}" ;
@@ -40,7 +40,7 @@ else
 fi
 echo "-> use image name ${image_building_name} for publish";
 
-application_version="`docker inspect -f '{{ index .Config.Labels "application.php.version"}}' ${image_building_name}`";
+application_version=`docker inspect -f '{{ index .Config.Labels "application.php.version"}}' ${image_building_name}`;
 
 if [[ -z "$VERSION" ]]; then
   # no fixed application version => latest build
@@ -75,7 +75,6 @@ echo "-> use image tags ${image_tags}"
  for tag in $image_tags; do
    image_final_tags+=("${image_tags_prefix}${tag}");
  done
-image_final_tags="7.3-alpine3.11-fpm-latest";
 
 image_final_tags="$(echo -n "${image_final_tags[*]}" | tr ' ' '\n' | uniq | tr '\n' ' ')";
 echo "-> use final image tags list ${image_final_tags}";
@@ -84,8 +83,8 @@ echo "-> use final image tags list ${image_final_tags}";
 for tag in $image_final_tags; do
   if echo "$tag" | grep -q "$image_version"; then
     echo "-? check if image version $image_version already exists in registry" ;
-    if curl -s "https://hub.docker.com/v2/repositories/${username}/${repo}/tags/?page_size=100" | grep -q '"name": "${tag}"'; then
-      echo "ERROR: Tag '${tag}' for image version '$image_version' already exists in registry" 1>&2 ;
+    if curl -s "https://hub.docker.com/v2/repositories/${username}/${repo}/tags/?page_size=100" | grep -q "name: ${tag}"; then
+      echo "ERROR: Tag ${tag} for image version $image_version already exists in registry" 1>&2 ;
       exit 0 ;
     fi
   fi
@@ -96,9 +95,9 @@ echo "$DOCKERHUB_REGISTRY_PASSWORD" | docker login --username="$DOCKERHUB_REGIST
 
 ## Push images
 for tag in $image_final_tags; do
-  echo "=> tag image '${image_building_name}' as '${DOCKER_IMAGE}:${tag}'" ;
+  echo "=> tag image ${image_building_name} as ${DOCKER_IMAGE}:${tag}" ;
   docker tag "${image_building_name}" "${DOCKER_IMAGE}:${tag}" ;
-  echo "=> push image '${DOCKER_IMAGE}:${tag}'" ;
+  echo "=> push image ${DOCKER_IMAGE}:${tag}" ;
   docker push "${DOCKER_REPO}:${tag}" ; 
 done
 
